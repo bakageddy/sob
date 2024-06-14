bcb22232e2a0dc84a18972d05beee97d0ded27f9
Dinesh Kumar <gdineshda@gmail.com>
2024-06-14 19:12:49 +0530

    Make it polished

--- main.go
+++ main.go
@@ -9,8 +9,9 @@ import (
 )
 
 type AppState struct {
-	Start int64 `json:"start"`
-	reset bool
+	Start     int64 `json:"start"`
+	reset     bool
+	StatePath string
 }
 
 var app AppState
@@ -18,12 +19,14 @@ var app AppState
 func init() {
 	flag.BoolVar(&app.reset, "restart", false, "Restart streak")
 	flag.Int64Var(&app.Start, "start_date", -1, "Set Start Date of Streak in UTC Time Format")
+	flag.StringVar(&app.StatePath, "path", "$HOME/.local/state/sob.state", "Set Default Path to save history")
 }
 
 func save_to_file(app AppState) error {
-	f, err := os.OpenFile("./state.json", os.O_WRONLY | os.O_TRUNC, os.ModePerm)
+	app.StatePath = os.ExpandEnv(app.StatePath)
+	f, err := os.OpenFile(app.StatePath, os.O_CREATE|os.O_RDWR, 0644)
 	if err != nil {
-		return err
+		fmt.Println("Error: ", err)
 	}
 	defer f.Close()
 	enc := json.NewEncoder(f)
@@ -32,10 +35,13 @@ func save_to_file(app AppState) error {
 }
 
 func load_from_file() (*AppState, error) {
-	f, err := os.Open("./state.json")
+	app.StatePath = os.ExpandEnv(app.StatePath)
+	f, err := os.Open(app.StatePath)
 	if err != nil {
 		return nil, err
 	}
+	defer f.Close()
+
 	var state AppState
 	dec := json.NewDecoder(f)
 	err = dec.Decode(&state)
@@ -47,19 +53,28 @@ func load_from_file() (*AppState, error) {
 
 func main() {
 	flag.Parse()
-	if (app.reset != true && app.Start == -1) {
-		app, err := load_from_file()
-		if err == nil {
-			t := time.Unix(int64(app.Start), 0)
-			fmt.Println("Streak: ", time.Since(t))
-		} else {
-			fmt.Println("Error: ", err)
+	if app.reset == true {
+		if app.Start == -1 {
+			app.Start = time.Now().Unix()
 		}
-	} else if (app.reset == true || app.Start != -1) {
-		app.Start = time.Now().Unix()
 		err := save_to_file(app)
 		if err != nil {
-			fmt.Println(err)
+			fmt.Println("Error trying to save file: ", err)
+			os.Exit(1)
 		}
+	} else {
+		app, err := load_from_file()
+		if err != nil {
+			fmt.Println("Error trying to load file: ", err)
+			fmt.Println("Consider restarting your counter")
+			os.Exit(1)
+		}
+		t := time.Unix(app.Start, 0)
+		duration := time.Since(t)
+		hours := int64(duration.Hours())
+		days := hours / 24
+		hours = hours % 24
+
+		fmt.Printf("Days: %d Hours: %d\n", days, hours)
 	}
 }
